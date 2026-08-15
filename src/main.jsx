370e3e13cdf0d721a5a98ebbd67abfd302d1d9d4
Jamison85 <jamisonr85@gmail.com>
2026-08-15 17:06:12 -0500

    Load detailed task guide styling

--- src/main.jsx
+++ src/main.jsx
@@ -4,4 +4,5 @@ import App from "./App";
 import "./styles.css";
 import "./character.css";
 import "./character-render-fix.css";
+import "./task-detail.css";
 ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><App /></React.StrictMode>);
