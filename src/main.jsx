32cfb73c2546699b66007a074994fad94c6e2325
Jamison85 <jamisonr85@gmail.com>
2026-08-15 14:03:52 -0500

    Load reliable coworker render override

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./character.css";
+import "./character-render-fix.css";
 ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><App /></React.StrictMode>);
